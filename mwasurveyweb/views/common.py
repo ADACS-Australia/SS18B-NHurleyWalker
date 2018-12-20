7e4dc2fa70bb822b3473d2726976cc7ff7daf38a
Shibli Saleheen <ssaleheen@swin.edu.au>
2018-12-20 16:59:43 +1100

    dynamically displaying buttons on the landing page

--- mwasurveyweb/views/common.py
+++ mwasurveyweb/views/common.py
@@ -5,6 +5,7 @@ Distributed under the MIT License. See LICENSE.txt for more info.
 from django.shortcuts import render
 
 from ..models import (
+    Colour,
     SkyPlotsConfiguration,
     SkyPlot,
 )
@@ -19,11 +20,32 @@ def index(request):
 
     sky_plots = SkyPlot.objects.all()
 
+    colours = Colour.objects.all()
+
+    buttons = []
+
+    for colour in colours:
+        labels = []
+
+        sky_plot_configurations = SkyPlotsConfiguration.objects.filter(colour=colour)
+
+        for sky_plot_configuration in sky_plot_configurations:
+            labels.append(sky_plot_configuration.observation_status.capitalize())
+
+        buttons.append(
+            dict(
+                colour=colour,
+                name='status_{}'.format(colour.name),
+                display_text=', '.join(labels),
+            )
+        )
+
     return render(
         request,
         "mwasurveyweb/welcome.html",
         {
             'sky_plots': sky_plots,
+            'buttons': buttons,
         }
     )
 
