64834ebe8c78412254617d8b10a7daf60646b7ef
Shibli Saleheen <ssaleheen@swin.edu.au>
2019-01-09 13:04:32 +1100

    code clean up and template filters documentation

--- mwasurveyweb/templatetags/template_filters.py
+++ mwasurveyweb/templatetags/template_filters.py
@@ -20,4 +20,9 @@ def get_item(dictionary, key):
 
 @register.filter
 def field_type(field):
+    """
+    Returns the field type of an input
+    :param field: input field
+    :return: string representing the class name
+    """
     return field.field.widget.__class__.__name__
